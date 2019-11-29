f721707983aed5820ed190509bc875e4f081d6e4
masayaShinoda- <46096911+masayaShinoda@users.noreply.github.com>
2019-11-29 10:04:58 +0700

    fixed tablet nav colors

--- wis-news-site-tablet.js
+++ wis-news-site-tablet.js
@@ -7,14 +7,14 @@ function closeNavTablet() {
     document.getElementById('nav-overlay-tablet').style.width = "0vmin";
 }
 function colorChangeBurgersTablet() {
-    document.getElementById('burgerTop').style.background = "#3B4259";
-    document.getElementById('burgerMid').style.background = "#3B4259";
-    document.getElementById('burgerBottom').style.background = "#3B4259";
-    document.getElementById('openNavButton').style.background = "white";
+    document.getElementById('burgerTopTablet').style.background = "#3B4259";
+    document.getElementById('burgerMidTablet').style.background = "#3B4259";
+    document.getElementById('burgerBottomTablet').style.background = "#3B4259";
+    document.getElementById('openNavButtonTablet').style.background = "white";
 }
 function colorChangeBurgersReverseTablet() {
-    document.getElementById('burgerTop').style.background = "white";
-    document.getElementById('burgerMid').style.background = "white";
-    document.getElementById('burgerBottom').style.background = "white";
-    document.getElementById('openNavButton-tablet').style.background = "#3B4259";
+    document.getElementById('burgerTopTablet').style.background = "white";
+    document.getElementById('burgerMidTablet').style.background = "white";
+    document.getElementById('burgerBottomTablet').style.background = "white";
+    document.getElementById('openNavButtonTablet').style.background = "#3B4259";
 }
