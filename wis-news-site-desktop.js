716e81ab72f5dfe20446c021cf206a7e7b8cf3dc
masayaShinoda- <46096911+masayaShinoda@users.noreply.github.com>
2020-01-21 14:39:23 +0700

    fixed banner

--- wis-news-site-desktop.js
+++ wis-news-site-desktop.js
@@ -1,6 +1,6 @@
 function logoChangeColor() {
-  document.getElementById('wis-banner-image').src = "./images/logos/logos v2-04.png";
+  document.getElementById('wis-banner-image').src = "./images/logos/logos-v2-04.png";
 }
 function logoChangeColorReverse() {
-  document.getElementById('wis-banner-image').src = "./images/logos/logos v2-03.png";
+  document.getElementById('wis-banner-image').src = "./images/logos/logos-v2-03.png";
 }
